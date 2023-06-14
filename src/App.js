e5cbf8ba2d5c16ae919de38d06059b8b01fa5a96
helloworld37773 <helloworld37773@gmail.com>
2023-06-14 11:50:11 +0900

    feat: todo 목록 전체 삭제기능 추가

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ function App() {
     { id: 4, title: "할일 4", completed: true },
   ]);
 
+  const handleRemoveClick = () => {
+    setTodoData([]);
+  }
+
   //이벤트 핸들러
   // const handleClick = _id => {
   //   // 전달된 ID를 검색해서 목록에서 제거
@@ -28,15 +32,16 @@ function App() {
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-300">
       <div className="w-4/5 p-6 bg-white rounded-[6px] shadow">
-        <div>
-          <div className="flex justify-between mb-3">
-            <h1 className="text-center w-3/4 text-2xl text-red-600 font-semibold">Firebase Todo-List</h1>
-          </div>
-          {/* 할일 메인 */}
-          <List todoData={todoData} setTodoData={setTodoData} />
-          {/* 할일 추가 */}
-          <Form todoData={todoData} setTodoData={setTodoData} />
+        <div className="flex justify-between mb-3">
+          <h1 className="text-center w-3/4 text-2xl text-red-600 font-semibold">
+            Firebase Todo-List
+          </h1>
+          <button onClick={handleRemoveClick} className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-400 text-[12px]">Delete All</button>
         </div>
+        {/* 할일 메인 */}
+        <List todoData={todoData} setTodoData={setTodoData} />
+        {/* 할일 추가 */}
+        <Form todoData={todoData} setTodoData={setTodoData} />
       </div>
     </div>
   );
